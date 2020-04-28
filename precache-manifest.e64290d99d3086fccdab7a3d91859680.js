ef57092668d1b7e0d33a466e795bbaabfd40253a
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2020-04-28 14:22:14 +0000

    deploy: 85bdf4696b18f6fd384ac43153508713207dc727

---
+++ precache-manifest.e64290d99d3086fccdab7a3d91859680.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0f600b759dec065bb251d3c603025f",
+    "url": "/bannerlord-toolkit/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0151503404a7e28bcbd35e63b2a497b6",
+    "url": "/bannerlord-toolkit/android-chrome-512x512.png"
+  },
+  {
+    "revision": "f1a640a1476538d7e8668ea25f4c1e2c",
+    "url": "/bannerlord-toolkit/apple-touch-icon.png"
+  },
+  {
+    "revision": "5229660387a5179c9cc9",
+    "url": "/bannerlord-toolkit/css/app.b9d1ac3a.css"
+  },
+  {
+    "revision": "a3cb8849b3a506d3971a",
+    "url": "/bannerlord-toolkit/css/chunk-vendors.e7c44726.css"
+  },
+  {
+    "revision": "4aaf334a058fe855d38b66174d09cf33",
+    "url": "/bannerlord-toolkit/favicon-16x16.png"
+  },
+  {
+    "revision": "51dcadce3c455caa7f5f577881142e22",
+    "url": "/bannerlord-toolkit/favicon-32x32.png"
+  },
+  {
+    "revision": "6ec4e807e0228802ec758c697d8f56c2",
+    "url": "/bannerlord-toolkit/index.html"
+  },
+  {
+    "revision": "5229660387a5179c9cc9",
+    "url": "/bannerlord-toolkit/js/app.46a61b10.js"
+  },
+  {
+    "revision": "a3cb8849b3a506d3971a",
+    "url": "/bannerlord-toolkit/js/chunk-vendors.1b39a825.js"
+  },
+  {
+    "revision": "c70f303c53b836be35e0d6e569700d4b",
+    "url": "/bannerlord-toolkit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bannerlord-toolkit/robots.txt"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/bannerlord-toolkit/site.webmanifest"
+  }
+]);
